05b4cb8436a3810d7fc2400e9505898aa6091718
Kass-S <kassiespacek101@gmail.com>
2025-03-24 01:09:34 -0700

    maping for week working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
   const [currentImgIcon, setCurrentImgIcon] = useState('');
   const [todaysWeather, setTodaysWeather] = useState('');
 
-  const [weekTemp, setWeekTemp] = useState(0);
-  const [weekImgIcon, setWeekImgIcon] = useState('');
-  const [weekWeather, setWeekWeather] = useState('');
+  // const [weekTemp, setWeekTemp] = useState(0);
+  // const [weekImgIcon, setWeekImgIcon] = useState('');
+  const [weekWeather, setWeekWeather] = useState<any[]>([]);
     
     
   const WeatherCall = async (cityInput: string) => {
@@ -36,10 +36,12 @@ export default function Home() {
       setCurrentImgIcon(currentData.weather[0].icon);
       setTodaysWeather(currentData.weather[0].description);
 
-      // map it so the proper info displays starts at 4 then increments by 8 in list
-      setWeekTemp(Math.round(data5Day.list[4].main.temp));
-      setWeekImgIcon(data5Day.list[4].weather[0].icon);
-      setWeekWeather(data5Day.list[4].weather[0].description);
+      
+      const filter5Day = data5Day.list.filter((list, index) => index % 8 == 0);
+
+      // setWeekTemp(Math.round(data5Day.list[4].main.temp));
+      // setWeekImgIcon(data5Day.list[4].weather[0].icon);
+      setWeekWeather(filter5Day.slice(0, 5));
     }
 
     
@@ -54,12 +56,12 @@ export default function Home() {
 
   return (
     <div className="m-0 min-h-screen min-w-screen bg-[url(/assets/weatherbg.png)] bg-no-repeat bg-cover font-farro float-right">
-      <div className="grid sm:grid-rows-3 sm:grid-cols-2 md:mx-36 mt-10">
+      <div className="grid sm:grid-rows-3 sm:grid-cols-2 mx-20 lg:mx-36 mt-10">
         <div className="row-span-3">
           <TodaysWeather city={currentCity} country={currentCountry} temp={currenttemp} minTemp={minTemp} maxTemp={maxTemp} imgIcon={currentImgIcon} todaysWeather={todaysWeather} />
         </div>
         <div className="sm:col-[2] flex justify-center sm:justify-end sm:max-h-10 mt-4 sm:mt-0">
-          <input type="text" placeholder="Search a City" className="light-yellow py-2 sm:pr-28 pl-10 rounded-md" onKeyDown={(event) => {
+          <input type="text" placeholder="Search a City" className="light-yellow py-2 lg:pr-28 pl-10 rounded-md" onKeyDown={(event) => {
             if(event.key === "Enter"){
               
               WeatherCall((event.target as HTMLInputElement).value);
@@ -69,9 +71,14 @@ export default function Home() {
         </div>        
       </div>
       
-      {/* need to map this out */}
-      <div className="sm:mx-17 mt-8 sm:mt-12">
-        <WeeksWeather temp={weekTemp} imgIcon={weekImgIcon} weekWeather={weekWeather} dayofweek="placeholder" />
+      
+      <div className="mx-20 lg:mx-36 mt-4 sm:mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+        {
+          weekWeather.map((day, index) => (
+            <WeeksWeather key={index} temp={Math.round(day.main.temp)} imgIcon={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} weekWeather={day.weather[0].description} dayofweek="placeholder" />
+          ))
+        }
+        
       </div> 
     </div>
   );
--- src/components/WeeksWeather.tsx
+++ src/components/WeeksWeather.tsx
@@ -13,11 +13,11 @@ interface WeeksWeatherInterface {
 const WeeksWeather = (props: WeeksWeatherInterface) => {
 
   return (
-    <div>
-        <Card className='max-w-[13rem] min-h-[25rem] max-h-[25rem] p-0 light-yellow border-0 mx-20'>
-            <CardHeader className='dark-yellow rounded-t-xl py-2'>
+    <div className='mt-4'>
+        <Card className='min-w-[13rem] max-w-[13rem] min-h-[25rem] max-h-[25rem] p-0 light-yellow border-0 flex justify-self-center'>
+            <CardHeader className='dark-yellow rounded-t-xl py-2 min-h-48 max-h-48'>
                 <div>
-                    <img className='flex justify-self-center pb-1' src={`https://openweathermap.org/img/wn/${props.imgIcon}@2x.png`} alt="weather icon" />
+                    <img className='flex justify-self-center pb-1' src={props.imgIcon} alt="weather icon" />
                     <p className='text-2xl text-center pt-1'>{props.weekWeather}</p>
                 </div>
             </CardHeader>
